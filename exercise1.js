37bb0c55de48690362b732ae96aaa88f637a032e
Puseletso Chuene <puseletsochuene88@gmail.com>
2024-06-25 20:17:42 +0200

    exercise 1 complete

--- exercise1.js
+++ exercise1.js
@@ -2,3 +2,4 @@
 let firstName = "Puseletso";
 let age = 23;
 let myVariable = "My name is Puseletso";
+let variable = "Batman", number = 5;
--- exercise1.ts
+++ exercise1.ts
@@ -4,3 +4,5 @@ let age: number = 23;
 
 let myVariable: any = "My name is Puseletso";
 
+let variable: string | number = "Batman",
+  number = 5;
